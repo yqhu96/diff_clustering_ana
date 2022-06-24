f755862a97bb6f5afd9a3c8ecff2fbe1544fe353
yqhu96 <98507177+yqhu96@users.noreply.github.com>
2022-06-24 17:28:09 +0800

    Update PCA.R

--- PCA.R
+++ PCA.R
@@ -1,14 +1,23 @@
 #学习网站：https://www.jianshu.com/p/2f71bc493042；#https://qastack.cn/stats/35561/imputation-of-missing-values-for-pca
 #正常seq矩阵的处理：确保matrix的numeric
-colnames(hp_DIFF) <- hp_DIFF[1,]
-hp_DIFF <- hp_DIFF[-1,]
+rownames(hp_DIFF) <- hp_DIFF[,1]
+hp_DIFF <- hp_DIFF[,-1]
 
 #缺失值的处理
 library(missMDA)
 pc_res <- imputePCA(hp_DIFF,method="EM",ncp=1)
 
+# 计算中值绝对偏差 (MAD, median absolute deviation)度量基因表达变化幅度
+# 在基因表达中，尽管某些基因很小的变化会导致重要的生物学意义，
+# 但是很小的观察值会引入很大的背景噪音，因此也意义不大。
+# 可以选择根据mad值过滤，取top 50， 500等做分析
+pc_res <- pc_res$completeObs
+mads <- apply(pc_res, 1, mad)
+pc_res <- pc_res[rev(order(mads)),]
+pc_res <- pc_res[1:500,]
+
 #matrix的转置，保证观测为样本，变量为打到的靶点
-adj_pc_res=as.data.frame(t(pc_res$completeObs))
+adj_pc_res=as.data.frame(t(pc_res))
 
 #grouplist的构建
 adj_pc_res$group=c("SXSM","SXSM","SXSM","CTRL","CTRL","CTRL")#group_list
